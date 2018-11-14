a663b7aeb4bbb9f2165619087231c58b651af070
Carlos Balladares <carlos@balladares.co>
2018-11-13 23:57:27 -0800

    adding readme

---
+++ README.md
@@ -0,0 +1,9 @@
+# Three js modern boilerplate
+
+This is a modern boilerplate for three js apps.
+
+## To run
+
+```shell
+$ npm run dev
+```
--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import * as THREE from "three";
 
-let scene = new THREE.Scene();
-let camera = new THREE.PerspectiveCamera(
+/* Setup renderer*/
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+/* Setup camera*/
+const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
 
-let renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+/* Setup scene */
+const scene = new THREE.Scene();
 
-let geometry = new THREE.BoxGeometry(2, 1, 1);
-let material = new THREE.MeshBasicMaterial({ color: 0x00ff00 }, false);
-let cube = new THREE.Mesh(geometry, material);
+const geometry = new THREE.BoxGeometry(2, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 }, false);
+const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 camera.position.z = 5;
 
+/* Animation */
 const animate = () => {
   requestAnimationFrame(animate);
 
